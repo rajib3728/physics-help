cb2153f4adf30dac8ca16809a705ba674a2471c1
Rajib Paul <53445533+rajib3728@users.noreply.github.com>
2022-04-06 13:43:01 +0530

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,128 @@
+function fun1()
+{   window.open("1.html")
+
+}
+function fun2()
+{
+    window.open("2.html")
+}
+function fun3()
+{
+    window.open("3.html")
+}
+function fun4()
+{
+    window.open("4.html")
+}
+function fun5()
+{
+    window.open("5.html")
+}
+function fun6()
+{
+    window.open("6.html")
+}
+function fun7()
+{
+    window.open("7.html")
+}
+function fun8()
+{
+    window.open("8.html")
+}
+function fun9()
+{
+    window.open("9.html")
+}
+function fun10()
+{
+    window.open("10.html")
+}
+function fun11()
+{
+    window.open("11.html")
+}
+function fun12()
+{
+    window.open("12.html")
+}
+function fun13()
+{
+    window.open("13.html")
+}
+function fun14()
+{
+    window.open("14.html")
+}
+function fun15()
+{
+    window.open("15.html")
+}
+function fun16()
+{
+    window.open("16.html")
+}
+function fun17()
+{
+    window.open("17.html")
+}
+function fun18()
+{
+    window.open("18.html")
+}
+function fun19()
+{
+    window.open("19.html")
+}
+function fun20()
+{
+    window.open("20.html")
+}
+function fun21()
+{
+    window.open("21.html")
+}
+function fun22()
+{
+    window.open("22.html")
+}
+function fun23()
+{
+    window.open("23.html")
+}
+function fun24()
+{
+    window.open("24.html")
+}
+function fun25()
+{
+    window.open("25.html")
+}
+function fun26()
+{
+    window.open("26.html")
+}
+function fun27()
+{
+    window.open("27.html")
+}
+function fun28()
+{
+    window.open("28.html")
+}
+function fun29()
+{
+    window.open("29.html")
+}
+function fun30()
+{
+    window.open("30.html")
+}
+function fun31()
+{
+    window.open("31.html");
+}
+function fun32()
+{
+    window.open("login.html")
+}
